164979fcec58f7f6c2ef426477d9a360f38bb6a5
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-06-10 19:40:57 +0530

    Variables

---
+++ variables.js
@@ -0,0 +1,37 @@
+// JavaScript Variables
+// Variables are Containers for Storing Data
+// var firstName = 100;
+
+
+// let firstName = 500;
+
+
+// // const firstName = 500;
+
+
+
+
+// var a = 100;
+
+// var a = 1000;
+
+// console.log(a);
+
+
+
+
+// let a = 500;
+
+//  a = 1000;
+
+// console.log(a);
+
+
+
+// const a = 500;
+// const A = 500;
+
+
+
+// console.log(a);
+
