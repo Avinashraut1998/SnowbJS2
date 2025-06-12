1f9e492e6faa2526d58b465cdf483284c2ec18bd
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-06-12 20:16:57 +0530

    Strings

---
+++ string.js
@@ -0,0 +1,120 @@
+// String 
+
+
+// let firstname = "Virat";
+
+
+// let res = firstname.length
+
+// console.log(res);
+
+
+
+//trim()
+// let str = "           String         ";
+
+// console.log(str);
+
+
+// let res = str.trim()
+
+// console.log(res);
+
+
+
+// toUpperCase()
+// let Fname = "rahul";
+
+// let res = Fname.toUpperCase()
+
+// console.log(res);
+
+
+
+// toLowerCase()
+// let fname = "RAHUL";
+
+// let res = fname.toLowerCase()
+
+// console.log(res);
+
+
+
+//indexOf()
+// let str = "i love coding"
+
+// let res = str.indexOf("coding")
+// console.log(res);
+
+
+// console.log(str);
+
+
+
+
+// method chaining
+
+// let studentName = "         mangesh";
+
+// let res = studentName.trim().toUpperCase()
+
+// console.log(res);
+
+
+
+// slice()
+
+// let str = "I Love Coding"
+
+// let res = str.slice(2, 6);
+
+// console.log(res);
+
+
+
+// replace()
+
+// let str = "I Love Coding"
+
+// let res = str.replace("Coding", "Javascript")
+
+// console.log(res);
+
+// repeat()
+
+// let str = "I Love Coding"
+
+// let res = str.repeat(5)
+
+// console.log(res);
+
+// let str = new String("I Love Coding")
+
+// console.log(str);
+
+
+
+
+// let str = 'Chetan'
+
+// console.log(typeof str);
+
+// let a = 500;
+// let b = 1000;
+
+
+// let str = `This is String Litrals ${a * b} value of a is ${a} and value of b is ${b}  
+// `
+
+// console.log(str);
+
+
+
+
+
+
+
+
+
+
+
