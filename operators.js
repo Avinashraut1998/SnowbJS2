a095783eb131e3df953996b99d314688bb75cf1b
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-06-18 09:12:08 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,100 @@
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Remainder)
+// ++	Increment
+// --	Decrement
+
+
+
+// +	Addition
+// let a = 50;
+// let b = 100;
+
+// let res = a + b;
+
+// console.log(res);
+
+
+// -	Subtraction
+
+// let a = 50;
+// let b = 20;
+
+// let res = a - b;
+
+// console.log(res);
+
+
+// *	Multiplication
+
+// let a = 55;
+// let b = 100;
+
+// let res = a * b;
+
+// console.log(res);
+
+
+
+// /	Division
+
+// let a = 12;
+// let b = 6;
+
+
+// let res = a / b;
+// console.log(res);
+
+
+// **	Exponentiation (ES2016)
+
+// let a = 10;
+
+// let b = 2;
+
+// let res = a ** b;
+
+// console.log(res);
+
+
+// %	Modulus (Remainder)
+
+// let a = 275;
+
+// let b = 10;
+
+// let res = a % b;
+
+// console.log(res);
+
+
+
+// // ++	Increment
+
+// let a = 5;
+
+// a++;  // a = a + 1
+
+// console.log(a);
+
+
+
+
+
+// // --	Decrement
+
+
+// let b = 5;
+
+// b--;  // b = b - 1;
+
+// console.log(b);
+
+
+
+
+
+
