17249fb839b65c11da71da6860d7bcbd01284773
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-06-19 19:48:51 +0530

    array

---
+++ array.js
@@ -0,0 +1,172 @@
+// An array is a special variable, which can hold more than one value:
+
+// let abc = []
+
+
+// let car1 = "Saab";
+// let car2 = "Volvo";
+// let car3 = "BMW";
+
+
+// let cars = ["Saab", "Volvo", "BMW"]  /// array
+
+// console.log(cars);
+
+// let myArr = new Array("Saab", "Volvo", "BMW")
+
+// console.log(myArr);
+
+// let emptyArray = [];
+
+// // emptyArray = 1;
+
+// emptyArray[0] = "One";
+// emptyArray[1] = "Two";
+// emptyArray[2] = "Three";
+// emptyArray[3] = "Four";
+// emptyArray[4] = "Five";
+
+// console.log(emptyArray);
+
+// let arr = ["One", "Two", "Three", "Four", "Five", "Six"]
+
+// console.log(arr.length);
+
+// Array Methods
+
+//push()
+
+// let arr = ["One", "Two", "Three", "Four", "Five", "Six"]
+
+// let res = arr.push("Seven")
+// arr.push("Eight")
+// arr.push("Nine")
+// arr.push("Ten")
+// console.log(arr);
+
+//pop()
+
+
+// let arr = ["One", "Two", "Three", "Four", "Five", "Six"]
+
+// console.log(arr);
+
+// arr.pop()
+
+// console.log(arr);
+
+
+// arr.pop();
+
+// console.log(arr);
+// arr.pop();
+
+// console.log(arr);
+// arr.pop();
+
+// console.log(arr);
+// arr.pop();
+
+// console.log(arr);
+// arr.pop();
+
+// console.log(arr);
+
+//toString()
+
+// let arr = ["One", "Two", "Three", "Four", "Five", "Six"]
+
+// console.log(arr);
+
+// console.log(arr.toString());
+
+// concat()
+
+// let arr1 = ["One", "Two", "Three", "Four", "Five", "Six"];
+
+// let arr2 = ["Seven", "Eight", "Nine", "Ten", "Eleven"];
+
+// let res = arr1.concat(arr2);
+
+// console.log(res);
+
+
+// unshift()
+
+// let arr1 = ["One", "Two", "Three", "Four", "Five", "Six"];
+
+// let res = arr1.unshift("Seven");
+
+// console.log(arr1);
+
+// shift()
+
+//  let arr1 = ["One", "Two", "Three", "Four", "Five", "Six"];
+
+//  arr1.shift()
+
+//  console.log(arr1);
+
+
+// slice()
+
+//  let arr1 = ["One", "Two", "Three", "Four", "Five", "Six"];
+
+//   console.log(arr1)
+
+//  console.log( arr1.slice(1, 4));
+
+
+//splice()
+
+//  let arr1 = ["One", "Two", "Three", "Four", "Five", "Six"];
+
+//  console.log(arr1.splice(2, 3));
+ 
+
+// let arr1 = ["One", "Two", "Three", "Four", "Five"];
+
+// let arr2 = ["Six", "Seven", "Eight", "Nine", "Ten"];
+
+// let arr3 = ["11", "12", "13", "14", "15"];
+
+// console.log(arr1);
+// console.log(arr2);
+
+// let res = arr1.concat(arr2, arr3);
+// console.log(res);
+
+
+// let arr1 = ["One", "Two", "Three", "Four", "Five"];
+
+// let arr2 = ["Six", "Seven", "Eight", "Nine", "Ten"];
+
+// let arr3 = ["11", "12", "13", "14", "15"];
+
+
+// let res = [...arr1, ...arr2, ...arr3]
+
+// console.log(res);
+
+
+
+ 
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
