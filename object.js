6895eee489b4c64b6e2205805a81821930f65438
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-06-20 20:08:17 +0530

    Objects

---
+++ object.js
@@ -0,0 +1,130 @@
+// var student = {
+//      firstName : "Vijay",
+//      lastName : "Patil",
+//      rollNo : 1010,
+// }
+
+// console.log(student);
+
+
+// let studentInfo = {};
+
+// studentInfo.fNmae = "Tushar"; 
+
+// console.log(studentInfo);
+
+
+// const obj = {
+//     fName: "Tushar",
+//     lname: "Kale",
+//     rollNo: 1010,
+//     pinCode: 444508,
+//     isActive: true,
+// }
+
+
+// console.log(obj);
+
+// console.log(obj.fName);
+// console.log(obj.lname);
+// console.log(obj.rollNo);
+// console.log(obj.pinCode);
+
+
+// let obj2 = new Object( {
+//     FirstName : "Amit",
+// })
+
+// console.log(obj2);
+
+
+
+
+// const obj = {
+//     fName: "Tushar",
+//     lname: "Kale",
+//     rollNo: 1010,
+//     pinCode: 444508,
+//     isActive: true,
+// }
+
+// // updating values
+// obj.pinCode = 505055;
+// obj.lname = "Patil";
+// obj.rollNo = 1020;
+
+
+// console.log(obj);
+
+
+// const obj = {
+//     fName: "Tushar",
+//     lname: "Kale",
+//     rollNo: 1010,
+//     pinCode: 444508,
+//     isActive: true,
+// }
+
+// let res = Object.values(obj);
+
+// console.log(res.toString());
+
+
+// const obj = {
+//     fName: "Tushar",
+//     lname: "Kale",
+//     rollNo: 1010,
+//     pinCode: 444508,
+//     isActive: true,
+// }
+
+// let res = Object.keys(obj);
+// console.log(res.join(" "));
+
+
+
+
+// const obj = {
+//     fName: "Tushar",
+//     lname: "Kale",
+//     rollNo: 1010,
+//     isActive: true,
+//     obj2 : {
+//         Address : "PLot No 35, Simti Road, Thane",
+//         pinCode: 444508,
+//         PhoneNumber : 9988778854,
+//         obj3 : {
+//             ClassName: "10A",
+//             ClassGrade: "A+1",
+//             ClassTeacherName: "Smita Patil Mam"
+//         }
+//     }
+// }
+
+// let res = obj.obj2.obj3.ClassTeacherName
+
+// console.log(res);
+
+
+// const obj = {
+//     fName: "Tushar",
+//     lname: "Kale",
+//     rollNo: 1010,
+//     isActive: true,
+//     Address: "PLot No 35, Simti Road, Thane",
+//     pinCode: 444508,
+//     PhoneNumber: 9988778854,
+//     ClassName: "10A",
+//     ClassGrade: "A+1",
+//     ClassTeacherName: "Smita Patil Mam"
+// }
+
+
+// const {Address, pinCode, ClassTeacherName, ClassName} = obj;
+
+// console.log(ClassName);
+
+
+
+
+
