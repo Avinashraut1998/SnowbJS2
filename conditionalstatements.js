1a2dd2ad484834f716075a326047bf38fc7ec25b
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-06-24 20:58:11 +0530

    Conditional statements

---
+++ conditionalstatements.js
@@ -0,0 +1,104 @@
+// Conditional Statements
+
+//Conditional statements are used to perform different actions based on different conditions.
+
+// if( condition ){
+//   // Logic 
+// } else{
+//     // If condition is false
+// }
+
+
+// if(true) {
+//     console.log("Condition is true");
+    
+// }else{
+//     console.log("Condition is false")
+// }
+
+
+// let age = 17;
+
+// if(age >= 18){
+//    console.log("User Can Vote");
+   
+// }else{
+//     console.log("User Can't Vote");
+    
+// }
+
+
+// let age = 11;
+
+// if(age >= 18){
+//    console.log("User Can Vote");
+   
+// }else if(age < 12){
+//     console.log("User Can't Vote because he is minior");
+    
+// }else{
+//     console.log("User Can't Vote");
+    
+// }
+
+
+// let age = 21;
+
+// if (age >= 18) {
+//     console.log("Can vote");
+   
+// }
+
+
+//ternary operator
+
+// condition ? true : false 
+
+// let age = 20;
+
+// age <= 18 ? console.log("Age is greater than 18") : console.log("Age is less than 18")
+
+// let day = new Date();
+// console.log(day.getDay());
+
+
+
+
+// switch (new Date().getDay()) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//      day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+// }
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+    
+    
