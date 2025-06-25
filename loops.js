cb85faefaab8bcb00d2106640535ba154e33caab
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-06-25 19:54:59 +0530

    Loops

---
+++ loops.js
@@ -0,0 +1,57 @@
+// for loop
+
+// for(let i = 1; i <= 10; i++) {
+//   console.log("value is :", i);
+//    for(let j = 1; j <= 10; j++){
+//     console.log(i * j);
+    
+//    }
+// }
+// console.log("OUT");
+
+//while loop
+
+// let i = 1;
+// while(i <= 10){
+//   console.log(i * 2);
+//   i++;
+// }
+
+
+// let players = ["Virat", "Rohit", "Pant", "Iyer", "Bhumrah", "Shami", "Gill"];
+
+// for (const element of object) {
+    
+// }
+
+//  for(const player of players) {
+//      console.log(`The Name of Player is : ${player}`)
+//  }
+
+
+// array.forEach(element => {
+    
+// });
+
+
+// players.forEach((player) => {
+//    console.log(player)
+// })
+
+
+
+// let lang = {
+//     js : "JavaScript",
+//     ts : "TypeScript",
+//     jv : "Java",
+//     py : "Python"
+// }
+
+
+// // for (const key in object) {
+   
+// // }
+
+// for(const key in lang) {
+//     console.log(`The actual name of ${key} is ${lang[key]}`)
+// }
